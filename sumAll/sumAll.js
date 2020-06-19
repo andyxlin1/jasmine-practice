bcc4e1f6503ffffa1cc569bebd7b0eee90f6ffe3
Andy Lin <andyxlin.1@gmail.com>
2020-06-19 03:17:00 -0700

    Finished sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(firstBound, secondBound) {
+    let returnNumber = 0;
+    if (typeof firstBound != 'number' || typeof secondBound != 'number') {
+        return 'ERROR';
+    }
+    if (firstBound < 0 || secondBound < 0) {
+        return 'ERROR';
+    }
+    if (firstBound > secondBound) {
+        let swap = firstBound;
+        firstBound = secondBound;
+        secondBound = swap;
+    }
 
+    for (let i = firstBound; i <= secondBound; i++) {
+        returnNumber+= i;
+    }
+    return returnNumber;
 }
 
 module.exports = sumAll
